06a2bc8156a049be163dc20da06be9bb463acc86
Julian Merlo <merlo_julian@hotmail.com>
2022-11-09 00:50:56 -0300

    PreEntrega1Merlo

---
+++ src/components/CartWidget.js
@@ -0,0 +1,10 @@
+function cartWidget() {
+  return (
+    <button className="nav-link " id="open" href="#">
+      Carrito <i className="bi bi-cart"></i>
+      <span>0</span>
+    </button>
+  );
+}
+
+export default cartWidget;
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,9 @@
+function ItemListContainer({ name }) {
+  return (
+    <div className="alert alert-success" role="alert">
+      <h2>{`Bienvenido ${name} a la tienda del CAFE!`}</h2>
+    </div>
+  );
+}
+
+export default ItemListContainer;
